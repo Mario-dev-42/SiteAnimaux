d335bf9120c584acb8daf0e2902dbc70877323fd
rayhearthdars <104355268+rayhearthdars@users.noreply.github.com>
2023-05-05 20:42:22 +0200

    Make corrections to caroussel, navbar and styling

---
+++ script.js
@@ -0,0 +1,37 @@
+const slide = ["./img/Baniere/slide0.jpeg", "./img/Baniere/slide1.jpg", "./img/Baniere/slide2.jpg", "./img/Baniere/slide3.jpg"];
+
+const carouselImages = document.querySelector(".caroussel-container");
+const prevButton = document.querySelector("#left");
+const nextButton = document.querySelector("#right");
+
+let currentIndex = 0;
+
+function showImage(index) {
+    carouselImages.style.backgroundImage = `url(${slide[index]})`;
+}
+
+function showNextImage() {
+    currentIndex = (currentIndex + 1) % slide.length;
+    showImage(currentIndex);
+}
+
+function showPrevImage() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = slide.length - 1;
+    }
+    showImage(currentIndex);
+}
+
+showImage(currentIndex);
+
+nextButton.addEventListener("click", showNextImage);
+prevButton.addEventListener("click", showPrevImage);
+
+// //////////////////////////////////////////////////////////
+
+// // autre facon
+
+// setInterval(() => {
+//     showNextImage(currentIndex)
+// }, 2000);
